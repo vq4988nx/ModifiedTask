37c98912d9971929e28f0ca403c4fe2bc5d830ce
vq4988nx <vq4988nx@go.minneapolis.edu>
2018-04-27 00:01:27 -0500

    Made some changes and created modal

--- src/main/java/com/jeff/hellowdb/HellowdbApplication.java
+++ src/main/java/com/jeff/hellowdb/HellowdbApplication.java
@@ -9,5 +9,6 @@ public class HellowdbApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HellowdbApplication.class, args);
 		java.lang.System.out.println("Hello");
+		java.lang.System.out.println("Howdy");
 	}
 }
